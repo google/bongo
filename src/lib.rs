3d7c0874cf4e353d6f9c32f932e0261b7c562b81
Brian Chin <brian.chin@gmail.com>
2018-08-11 10:55:47 -0700

    Partial implementation of choice set

--- src/lib.rs
+++ src/lib.rs
@@ -51,7 +51,10 @@ struct JuxtaposeBuilder {
 
 impl JuxtaposeBuilder {
     fn new(right: &Layout) -> Self {
-        JuxtaposeBuilder { right: right.clone(), memo: col::BTreeMap::new() }
+        JuxtaposeBuilder {
+            right: right.clone(),
+            memo: col::BTreeMap::new(),
+        }
     }
 
     fn juxtapose(&mut self, left: &Layout) -> Layout {
@@ -60,16 +63,23 @@ impl JuxtaposeBuilder {
         }
 
         let new_layout = match left.contents() {
-            LayoutContents::Text(t) => Layout::with_contents(LayoutContents::Juxtapose(t.clone(), self.right.clone())),
-            LayoutContents::Choice(first, second) =>
-                Layout::with_contents(LayoutContents::Choice(self.juxtapose(first), self.juxtapose(second))),
-            LayoutContents::Stack(top, bottom) =>
-                Layout::with_contents(LayoutContents::Stack(top.clone(), self.juxtapose(bottom))),
-            LayoutContents::Juxtapose(text, jux_right) =>
-                Layout::with_contents(LayoutContents::Juxtapose(text.clone(), self.juxtapose(jux_right))),
+            LayoutContents::Text(t) => {
+                Layout::with_contents(LayoutContents::Juxtapose(t.clone(), self.right.clone()))
+            }
+            LayoutContents::Choice(first, second) => Layout::with_contents(LayoutContents::Choice(
+                self.juxtapose(first),
+                self.juxtapose(second),
+            )),
+            LayoutContents::Stack(top, bottom) => {
+                Layout::with_contents(LayoutContents::Stack(top.clone(), self.juxtapose(bottom)))
+            }
+            LayoutContents::Juxtapose(text, jux_right) => Layout::with_contents(
+                LayoutContents::Juxtapose(text.clone(), self.juxtapose(jux_right)),
+            ),
         };
 
-        self.memo.insert(left.contents() as *const LayoutContents, new_layout.clone());
+        self.memo
+            .insert(left.contents() as *const LayoutContents, new_layout.clone());
         new_layout
     }
 }
@@ -83,6 +93,24 @@ enum ResolvedLayout {
 
 #[derive(Clone, Debug)]
 struct ResolvedLayoutRef(std::rc::Rc<ResolvedLayout>);
+
+impl ResolvedLayoutRef {
+    fn new(inner: ResolvedLayout) -> ResolvedLayoutRef {
+        ResolvedLayoutRef(rc::Rc::new(inner))
+    }
+
+    pub fn new_text(text: impl Into<String>) -> ResolvedLayoutRef {
+        ResolvedLayoutRef::new(ResolvedLayout::Text(text.into()))
+    }
+
+    pub fn new_horiz(left: ResolvedLayoutRef, right: ResolvedLayoutRef) -> ResolvedLayoutRef {
+        ResolvedLayoutRef::new(ResolvedLayout::Horiz(left, right))
+    }
+
+    pub fn new_vert(top: ResolvedLayoutRef, bottom: ResolvedLayoutRef) -> ResolvedLayoutRef {
+        ResolvedLayoutRef::new(ResolvedLayout::Vert(top, bottom))
+    }
+}
 
 #[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Debug)]
 struct KnotColumn(u16);
@@ -171,9 +199,17 @@ impl KnotSet {
         self.knots.keys().cloned().collect()
     }
 
-    fn knot_values_between(&self, begin: KnotColumn, end: KnotColumn) -> col::BTreeSet<KnotColumn> {
+    fn knot_values_between(
+        &self,
+        begin: KnotColumn,
+        end: Option<KnotColumn>,
+    ) -> col::BTreeSet<KnotColumn> {
+        let right_bound = match end {
+            Some(c) => ops::Bound::Excluded(c),
+            None => ops::Bound::Unbounded,
+        };
         self.knots
-            .range((ops::Bound::Included(begin), ops::Bound::Excluded(end)))
+            .range((ops::Bound::Included(begin), right_bound))
             .map(|(k, _)| *k)
             .collect()
     }
@@ -190,7 +226,9 @@ impl KnotSet {
         let mut knot_iter = self.knots.iter();
         let mut curr_col;
         let mut curr_data;
-        let (first_col, first_data) = knot_iter.next().expect("KnotSets should have at least one element.");
+        let (first_col, first_data) = knot_iter
+            .next()
+            .expect("KnotSets should have at least one element.");
         curr_col = *first_col;
         curr_data = first_data.clone();
         let mut result = Vec::with_capacity(self.knots.len());
@@ -224,6 +262,19 @@ struct KnotInterval {
     data: KnotData,
 }
 
+fn column_intervals(columns: &col::BTreeSet<KnotColumn>) -> Vec<(KnotColumn, Option<KnotColumn>)> {
+    let mut iter = columns.iter();
+    let first = iter.next().expect("Always at least a knot at 0");
+    let mut curr_start = *first;
+    let mut result = Vec::new();
+    for &col in iter {
+        result.push((curr_start, Some(col)));
+        curr_start = col;
+    }
+    result.push((curr_start, None));
+    result
+}
+
 struct KnotSetBuilder {
     margin: u16,
     overflow_cost: f32,
@@ -234,7 +285,7 @@ impl KnotSetBuilder {
     pub fn new_text(&self, text: impl Into<String>) -> KnotSet {
         let text_str = text.into();
         let text_len = text_str.len() as u16;
-        let layout = ResolvedLayoutRef(rc::Rc::new(ResolvedLayout::Text(text_str)));
+        let layout = ResolvedLayoutRef::new_text(text_str);
         let mut knots = col::BTreeMap::new();
         if text_len < self.margin {
             let flat_data = KnotData {
@@ -276,10 +327,10 @@ impl KnotSetBuilder {
             let top_data = top.knot_data_at(col);
             let bottom_data = bottom.knot_data_at(col);
             KnotData {
-                resolved_layout: ResolvedLayoutRef(rc::Rc::new(ResolvedLayout::Vert(
+                resolved_layout: ResolvedLayoutRef::new_vert(
                     top_data.resolved_layout.clone(),
                     bottom_data.resolved_layout.clone(),
-                ))),
+                ),
                 span: bottom_data.span,
                 intercept: top_data.intercept + bottom_data.intercept + self.newline_cost,
                 rise: top_data.rise + bottom_data.rise,
@@ -287,11 +338,79 @@ impl KnotSetBuilder {
         })
     }
 
-    pub fn new_horiz(&self, _left: KnotSet, _right: KnotSet) -> KnotSet {
-        unimplemented!()
+    pub fn new_horiz(&self, left: impl Into<String>, right: KnotSet) -> KnotSet {
+        let left = left.into();
+        let left_width = left.len() as u16;
+
+        let shifted_left_knot_values = right
+            .knot_values_between(KnotColumn(left_width), None)
+            .into_iter()
+            .map(|KnotColumn(col)| KnotColumn(col - left_width))
+            .collect();
+
+        let text_knot_set = self.new_text(left);
+        let new_knot_values: col::BTreeSet<_> = text_knot_set
+            .knot_values()
+            .union(&shifted_left_knot_values)
+            .cloned()
+            .collect();
+
+        KnotSet::new_knot_set(&new_knot_values, |col| {
+            let right_knot_col = KnotColumn(col.0 + left_width);
+            let left_data = text_knot_set.knot_data_at(col);
+            let right_data = right.knot_data_at(right_knot_col);
+
+            let sub_intercept_factor;
+            let sub_rise_factor;
+            if col.0 > self.margin {
+                sub_intercept_factor = (col.0 - self.margin) as f32 * self.overflow_cost;
+                sub_rise_factor = self.overflow_cost;
+            } else {
+                sub_intercept_factor = 0.0;
+                sub_rise_factor = 0.0;
+            }
+            KnotData {
+                resolved_layout: ResolvedLayoutRef::new_horiz(
+                    right_data.resolved_layout.clone(),
+                    left_data.resolved_layout.clone(),
+                ),
+                span: left_data.span + right_data.span,
+                intercept: left_data.intercept + right_data.intercept - sub_intercept_factor,
+                rise: left_data.rise + right_data.rise - sub_rise_factor,
+            }
+        })
     }
 
-    pub fn new_choice(&self, _choice1: KnotSet, _choice2: KnotSet) -> KnotSet {
+    pub fn new_choice(&self, choice1: KnotSet, choice2: KnotSet) -> KnotSet {
+        // Set "L" in the paper
+        let base_knots: col::BTreeSet<_> = choice1
+            .knot_values()
+            .union(&choice2.knot_values())
+            .cloned()
+            .collect();
+        let mut extra_knots = col::BTreeSet::new();
+        for (start, end_opt) in column_intervals(&base_knots) {
+            // Find the Chi_K value for this range.
+            let choice1_data = choice1.knot_data_at(start);
+            let choice2_data = choice2.knot_data_at(start);
+
+            let chi_k = (choice2_data.intercept - choice1_data.intercept)
+                / (choice1_data.rise - choice2_data.rise);
+
+            let intersect_delta = chi_k.ceil() as u16;
+
+            let in_range = match end_opt {
+                Some(k) => start.0 + intersect_delta < k.0,
+                None => false,
+            };
+
+            if in_range {
+                extra_knots.insert(KnotColumn(start.0 + intersect_delta));
+            }
+        }
+
+        let all_knots: col::BTreeSet<_> = base_knots.union(&extra_knots).cloned().collect();
+
         unimplemented!()
     }
 }
