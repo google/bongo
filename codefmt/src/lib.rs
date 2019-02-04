4d2aef8ded20fcc8290124d2e7747e05eeffa6d7
Brian Chin <brianchin@google.com>
2019-02-04 13:06:15 -0800

    Bring up to date to Rust 2018

--- codefmt/src/lib.rs
+++ codefmt/src/lib.rs
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#![feature(uniform_paths)]
 //! Simple efficient composable primitives for pretty printing.
 //!
 //! The implementation is derived from the paper ["A New Approach to Optimal
