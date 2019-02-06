348087cafc70892ed0f24f842c6d77b0c9e211e7
Brian Chin <brianchin@google.com>
2019-02-05 18:48:14 -0800

    Extract function to simplify calculate_nullables

--- bongo/src/grammar/nullables.rs
+++ bongo/src/grammar/nullables.rs
@@ -34,6 +34,38 @@ fn grammar_to_prods_with_heads<E: ElementTypes>(
   result
 }
 
+fn calculate_nullable_tree_nodes<E: ElementTypes>(
+  nullables: &BTreeMap<E::NonTerm, NullableInfo<E::Action>>,
+  prod: &Production<E>,
+) -> BTreeSet<TreeNode<E::Action, Void>> {
+  let mut node_args_set = BTreeSet::new();
+  node_args_set.insert(BTreeMap::new());
+  for prod_elem in prod.prod_elements() {
+    let nt = prod_elem.elem().as_nonterm();
+    let nt_nullable_info = nullables.get(nt).unwrap();
+    if let Some(id) = prod_elem.id() {
+      let mut new_node_args_set = BTreeSet::new();
+      for old_node_arg in &node_args_set {
+        for nullable_action in &nt_nullable_info.nullable_actions {
+          let mut new_node_arg = old_node_arg.clone();
+          new_node_arg.insert(
+            id.clone(),
+            TreeValue::Node(Box::new(nullable_action.clone())),
+          );
+          new_node_args_set.insert(new_node_arg);
+        }
+      }
+
+      node_args_set = new_node_args_set;
+    }
+  }
+
+  node_args_set
+    .into_iter()
+    .map(|node_args| TreeNode::new(prod.action().clone(), node_args))
+    .collect()
+}
+
 /// Calculate the nullable set of a grammar
 ///
 /// The nullable set of a grammar is the set of non-terminals in that grammar
@@ -43,41 +75,15 @@ pub fn calculate_nullables<E: ElementTypes>(
 ) -> BTreeMap<E::NonTerm, NullableInfo<E::Action>> {
   let prods_with_heads = grammar_to_prods_with_heads(g);
 
-  let mut nullable_nts: BTreeMap<E::NonTerm, NullableInfo<E::Action>> =
-    BTreeMap::new();
+  let mut nullable_nts = BTreeMap::new();
 
   loop {
     let mut changed = false;
 
     for prod_with_head in &prods_with_heads {
       if is_prod_nullable(&nullable_nts, prod_with_head.prod) {
-        let mut node_args_set: BTreeSet<
-          BTreeMap<Name, TreeValue<E::Action, Void>>,
-        > = BTreeSet::new();
-        node_args_set.insert(BTreeMap::new());
-        for prod_elem in prod_with_head.prod.prod_elements() {
-          let nt = prod_elem.elem().as_nonterm();
-          let nt_nullable_info = nullable_nts.get(nt).unwrap();
-          if let Some(id) = prod_elem.id() {
-            let mut new_node_args_set = BTreeSet::new();
-            for old_node_arg in &node_args_set {
-              for nullable_action in &nt_nullable_info.nullable_actions {
-                let mut new_node_arg = old_node_arg.clone();
-                new_node_arg.insert(
-                  id.clone(),
-                  TreeValue::Node(Box::new(nullable_action.clone())),
-                );
-                new_node_args_set.insert(new_node_arg);
-              }
-            }
-
-            node_args_set = new_node_args_set;
-          }
-        }
-
-        let new_nodes = node_args_set.into_iter().map(|node_args| {
-          TreeNode::new(prod_with_head.prod.action().clone(), node_args)
-        });
+        let new_nodes =
+          calculate_nullable_tree_nodes(&nullable_nts, prod_with_head.prod);
 
         let nullable_info = nullable_nts
           .entry(prod_with_head.head.clone())
